3c501b27ca9cbc2db6e9862d7c88cde9b4e7b977
kbtyc <66954011@qq.com>
2013-09-28 12:01:55 +0800

    del www

--- www/scripts/product/app.js
+++
@@ -1,8 +0,0 @@
-angular.module('product', ['ngRoute']).
-  config(function($routeProvider, $locationProvider) {
-  $routeProvider.
-      when('/product', {template: '/product', controller: ProductListCtrl, controllerAs: 'pclr'})
-	  .when('/product/:catalogId', {template: '/product/:catalogId', controller: ProductListCtrl, controllerAs: 'pclr'})
-	  .when('/product/:catalogId/:pageIndex', {template: '/product/:catalogId/:pageIndex', controller: ProductListCtrl, controllerAs: 'pclr'})
-	  .otherwise({redirectTo: '/product'});
-});
