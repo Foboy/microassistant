1ce38082a71b589cce4cfab1f33973e727098d87
wangxiao <yuwojiusi@163.com>
2013-09-28 17:21:04 +0800

    原型

---
+++ Prototype/登录_files/axurerp_pagespecificscript.js
@@ -0,0 +1,7 @@
+﻿for(var i = 0; i < 18; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u12'] = 'top';gv_vAlignTable['u4'] = 'center';gv_vAlignTable['u17'] = 'top';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u14'] = 'center';gv_vAlignTable['u15'] = 'top';gv_vAlignTable['u16'] = 'top';gv_vAlignTable['u6'] = 'center';gv_vAlignTable['u2'] = 'top';gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u7'] = 'top';
