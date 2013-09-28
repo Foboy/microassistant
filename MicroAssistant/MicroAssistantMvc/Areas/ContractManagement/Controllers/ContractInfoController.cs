e2cb3b6ba259be4ba1cdec65dd8893bd2c0f4e6c
zhengrunqiang <653260669@qq.com>
2013-09-28 14:34:43 +0800

    22

---
+++ MicroAssistant/MicroAssistantMvc/Areas/ContractManagement/Controllers/ContractInfoController.cs
@@ -0,0 +1,49 @@
+﻿using MicroAssistant.Meta;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MicroAssistantMvc.Areas.ContractManagement.Controllers
+{
+    public class ContractInfoController : Controller
+    {
+        //
+        // GET: /ContractManagement/ContractInfo/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 根据合同ID获取合同信息
+        /// </summary>
+        /// <param name="eid"></param>
+        /// <returns></returns>
+        public JsonResult GetContractInfoByEID(int eid)
+        {
+            return null;
+        }
+        /// <summary>
+        /// 添加合同
+        /// </summary>
+        /// <param name="contract"></param>
+        /// <returns></returns>
+        public JsonResult AddContractInfo(ContractInfo contract)
+        {
+            return null;
+        }
+        /// <summary>
+        /// 上传合同附件
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public JsonResult UploadContractAttachedFile(object file)
+        {
+            return null;
+        }
+
+
+    }
+}
---
+++ Prototype/确认付款_files/axurerp_pagespecificscript.js
@@ -0,0 +1,128 @@
+﻿for(var i = 0; i < 86; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u21'] = 'top';gv_vAlignTable['u51'] = 'top';document.getElementById('u25_img').tabIndex = 0;
+
+u25.style.cursor = 'pointer';
+$axure.eventManager.click('u25', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('客户信息.html');
+
+}
+});
+document.getElementById('u16_img').tabIndex = 0;
+
+u16.style.cursor = 'pointer';
+$axure.eventManager.click('u16', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('产品管理.html');
+
+}
+});
+gv_vAlignTable['u55'] = 'top';gv_vAlignTable['u46'] = 'center';gv_vAlignTable['u68'] = 'top';gv_vAlignTable['u48'] = 'top';gv_vAlignTable['u8'] = 'top';gv_vAlignTable['u32'] = 'center';gv_vAlignTable['u23'] = 'center';u62.tabIndex = 0;
+
+u62.style.cursor = 'pointer';
+$axure.eventManager.click('u62', function(e) {
+
+if (true) {
+
+	self.location.href='#';
+
+}
+});
+gv_vAlignTable['u62'] = 'top';gv_vAlignTable['u53'] = 'center';gv_vAlignTable['u85'] = 'center';gv_vAlignTable['u1'] = 'center';document.getElementById('u38_img').tabIndex = 0;
+
+u38.style.cursor = 'pointer';
+$axure.eventManager.click('u38', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('应付款.html');
+
+}
+});
+gv_vAlignTable['u7'] = 'center';gv_vAlignTable['u67'] = 'top';gv_vAlignTable['u30'] = 'center';gv_vAlignTable['u34'] = 'center';gv_vAlignTable['u64'] = 'center';document.getElementById('u19_img').tabIndex = 0;
+
+u19.style.cursor = 'pointer';
+$axure.eventManager.click('u19', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('销售管理.html');
+
+}
+});
+gv_vAlignTable['u49'] = 'top';gv_vAlignTable['u79'] = 'center';gv_vAlignTable['u81'] = 'top';gv_vAlignTable['u17'] = 'center';gv_vAlignTable['u15'] = 'top';gv_vAlignTable['u36'] = 'center';gv_vAlignTable['u66'] = 'top';gv_vAlignTable['u83'] = 'center';document.getElementById('u22_img').tabIndex = 0;
+
+u22.style.cursor = 'pointer';
+$axure.eventManager.click('u22', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('财务管理.html');
+
+}
+});
+gv_vAlignTable['u13'] = 'center';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u47'] = 'top';gv_vAlignTable['u77'] = 'center';gv_vAlignTable['u28'] = 'top';gv_vAlignTable['u20'] = 'center';gv_vAlignTable['u50'] = 'top';gv_vAlignTable['u24'] = 'top';gv_vAlignTable['u54'] = 'top';gv_vAlignTable['u39'] = 'center';gv_vAlignTable['u69'] = 'top';document.getElementById('u84_img').tabIndex = 0;
+
+u84.style.cursor = 'pointer';
+$axure.eventManager.click('u84', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('财务管理.html');
+
+}
+});
+gv_vAlignTable['u71'] = 'top';gv_vAlignTable['u61'] = 'top';gv_vAlignTable['u26'] = 'center';u65.tabIndex = 0;
+
+u65.style.cursor = 'pointer';
+$axure.eventManager.click('u65', function(e) {
+
+if (true) {
+
+	self.location.href='#';
+
+}
+});
+gv_vAlignTable['u65'] = 'top';gv_vAlignTable['u56'] = 'top';gv_vAlignTable['u5'] = 'center';document.getElementById('u82_img').tabIndex = 0;
+
+u82.style.cursor = 'pointer';
+$axure.eventManager.click('u82', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('财务管理.html');
+
+}
+});
+gv_vAlignTable['u42'] = 'center';document.getElementById('u33_img').tabIndex = 0;
+
+u33.style.cursor = 'pointer';
+$axure.eventManager.click('u33', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('财务管理.html');
+
+}
+});
+gv_vAlignTable['u72'] = 'top';u18.tabIndex = 0;
+
+u18.style.cursor = 'pointer';
+$axure.eventManager.click('u18', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('销售管理.html');
+
+}
+});
+gv_vAlignTable['u18'] = 'top';gv_vAlignTable['u80'] = 'top';gv_vAlignTable['u10'] = 'center';gv_vAlignTable['u40'] = 'top';gv_vAlignTable['u73'] = 'top';gv_vAlignTable['u44'] = 'center';gv_vAlignTable['u74'] = 'top';
