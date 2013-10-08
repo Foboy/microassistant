55dfdf05cde0eb4ce35f96a4fc18cd64c79b6d8c
xcoolray <285025600@qq.com>
2013-10-08 15:08:57 +0800

    前端更新

--- www/scripts/app.js
+++ www/scripts/app.js
@@ -1,8 +1,41 @@
 angular.module('microassistant', ['ngRoute']).
   config(function($routeProvider, $locationProvider) {
   $routeProvider
+  	  .when('/home', {templateUrl: 'partials/home.html', controller: HomeMainCtrl})
 	  .when('/product/:catalogId?/:pageIndex?', {templateUrl: 'partials/product.html', controller: ProductMainCtrl})
 	  .when('/sales/:steps?/:pageIndex?', {templateUrl: 'partials/sales.html', controller: SalesMainCtrl})
-	  .otherwise({redirectTo: '/product'});
+	  .when('/finance', {templateUrl: 'partials/finance.html', controller: FinanceMainCtrl})
+	  .otherwise({redirectTo: '/home'});
   	  $locationProvider.hashPrefix('!');
 }).value('$anchorScroll', angular.noop);
+
+function MainCtrl($scope, $routeParams, $http, $location){
+
+	$scope.$on('$routeChangeSuccess',function(){
+		$scope.checkpage();
+	});
+	
+	$scope.checkpage = function(){
+		if($location.path().indexOf('/home')==0)
+		{
+			$scope.page="home";
+		}
+		else if($location.path().indexOf('/product')==0)
+		{
+			$scope.page="product";
+		}
+		else if($location.path().indexOf('/sales')==0)
+		{
+			$scope.page="sales";
+		}
+		else if($location.path().indexOf('/finance')==0)
+		{
+			$scope.page="finance";
+		}
+		else
+		{
+			$scope.page="home";
+		}
+	};
+	$scope.checkpage();
+}
