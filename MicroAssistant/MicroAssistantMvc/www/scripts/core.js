1dcb8a6910b7210b93e26c0779584e5018c2f276
zhengrunqiang <653260669@qq.com>
2013-10-21 21:17:58 +0800

    提交

--- MicroAssistant/MicroAssistantMvc/www/scripts/core.js
+++ MicroAssistant/MicroAssistantMvc/www/scripts/core.js
@@ -5,6 +5,7 @@ $sitecore.urls.base = "";
 $sitecore.urls.add = function(name,url){
 	$sitecore.urls[name] = $sitecore.urls.base + url +"?timestamp="+ new Date().getTime();
 };
+$sitecore.urls.add("userLogin", "/UserManagement/User/Login");
 $sitecore.urls.add("productCat", "scripts/product/json/productCat.json");
 $sitecore.urls.add("productList", "scripts/product/json/productList.json");
 $sitecore.urls.add("productDetail", "scripts/product/json/productDetail.json");
