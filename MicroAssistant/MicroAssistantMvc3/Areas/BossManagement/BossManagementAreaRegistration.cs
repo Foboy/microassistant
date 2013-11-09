2b8fd3694dcc855f8f884d512715368f04cd387d
kbtyc <66954011@qq.com>
2013-11-09 15:25:14 +0800

    老板模式接口更新

---
+++ MicroAssistant/MicroAssistantMvc3/Areas/BossManagement/BossManagementAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MicroAssistantMvc.Areas.BossManagement
+{
+    public class BossManagementAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "BossManagement";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "BossManagement_default",
+                "BossManagement/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ MicroAssistant/MicroAssistantMvc3/Areas/BossManagement/Models/SalesOppReport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MicroAssistantMvc.Areas.BossManagement.Models
+{
+    public class SalesOppReport
+    {
+        public string month
+        { get; set; }
+        public int newc
+        { get; set; }
+        public int old
+        { get; set; }
+    }
+}
