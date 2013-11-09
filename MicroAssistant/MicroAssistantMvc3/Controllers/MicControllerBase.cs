5dbf31e8f5d002f9e5039559a569fff5440120bf
zhengrunqiang <653260669@qq.com>
2013-11-09 12:58:33 +0800

    dasd

--- MicroAssistant/MicroAssistantMvc3/Controllers/MicControllerBase.cs
+++ MicroAssistant/MicroAssistantMvc3/Controllers/MicControllerBase.cs
@@ -11,7 +11,7 @@ using System.Web.Security;
 
 namespace MicroAssistantMvc.Controllers
 {
-    public class MicControllerBase : Controller
+    public class MicControllerBase :Controller,IController
     {
         #region token
         private string currentToken;
@@ -100,6 +100,7 @@ namespace MicroAssistantMvc.Controllers
             ViewBag.CurrentToken = token;
             base.OnAuthorization(filterContext);
         }
-    
+
+
     }
 }
