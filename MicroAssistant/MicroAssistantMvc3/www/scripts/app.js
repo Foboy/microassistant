1f8b0257e600c98b053527f21f85209468b15b76
xcoolray <285025600@qq.com>
2013-11-23 15:32:54 +0800

    nested view

--- MicroAssistant/MicroAssistantMvc3/www/scripts/app.js
+++ MicroAssistant/MicroAssistantMvc3/www/scripts/app.js
@@ -1,21 +1,69 @@
-﻿angular.module('microassistant', ['ngRoute', 'ngLoadMask']).
-  config(['$provide', '$routeProvider', '$locationProvider',function ($provide, $routeProvider, $locationProvider) {
-      $routeProvider
-          .when('/home', { templateUrl: 'partials/home.html', controller: HomeMainCtrl })
-          .when('/user', { templateUrl: 'partials/userinfo.html', controller: UserMainCtrl })
-          .when('/mytimeshaft', { templateUrl: 'partials/mytimeshaft.html', controller: UserTimeShaftCtrl })
-          .when('/staffmangement', { templateUrl: 'partials/staffmangement.html', controller: StaffMangementCtrl })
-          .when('/changepassword', { templateUrl: 'partials/changepassword.html', controller: UserMainCtrl })
-          .when('/changeemail', { templateUrl: 'partials/changeemail.html', controller: UserMainCtrl })
-          .when('/enterpriseinfo', { templateUrl: 'partials/enterpriseinfo.html', controller: EnterPriseInfoCtrl })
-          .when('/product/:catalogId?/:pageIndex?', { templateUrl: 'partials/product.html', controller: ProductMainCtrl })
-          .when('/sales/:steps?/:pageIndex?', { templateUrl: 'partials/sales.html', controller: SalesMainCtrl })
-          .when('/finance/:steps?/:pageIndex?', { templateUrl: 'partials/finance.html', controller: FinanceMainCtrl })
-          .when('/client/:sorts?/:pageIndex?', { templateUrl: 'partials/client.html', controller: ClientMainCtrl })
-          .otherwise({ redirectTo: '/home' });
-      $locationProvider.hashPrefix('!');
+﻿angular.module('microassistant', ['ngRoute','ui.router', 'ngLoadMask']).
+  //config(['$provide', '$routeProvider', '$locationProvider',function ($provide, $routeProvider, $locationProvider) {
+  //    $routeProvider
+  //        .when('/home', { templateUrl: 'partials/home.html', controller: HomeMainCtrl })
+  //        .when('/user', { templateUrl: 'partials/userinfo.html', controller: UserMainCtrl })
+  //        .when('/mytimeshaft', { templateUrl: 'partials/mytimeshaft.html', controller: UserTimeShaftCtrl })
+  //        .when('/staffmangement', { templateUrl: 'partials/staffmangement.html', controller: StaffMangementCtrl })
+  //        .when('/changepassword', { templateUrl: 'partials/changepassword.html', controller: UserMainCtrl })
+  //        .when('/changeemail', { templateUrl: 'partials/changeemail.html', controller: UserMainCtrl })
+  //        .when('/enterpriseinfo', { templateUrl: 'partials/enterpriseinfo.html', controller: EnterPriseInfoCtrl })
+  //        .when('/product/:catalogId?/:pageIndex?', { templateUrl: 'partials/product.html', controller: ProductMainCtrl })
+  //        .when('/sales/:steps?/:pageIndex?', { templateUrl: 'partials/sales.html', controller: SalesMainCtrl })
+  //        .when('/finance/:steps?/:pageIndex?', { templateUrl: 'partials/finance.html', controller: FinanceMainCtrl })
+  //        .when('/client/:sorts?/:pageIndex?', { templateUrl: 'partials/client.html', controller: ClientMainCtrl })
+  //        .otherwise({ redirectTo: '/home' });
+  //    $locationProvider.hashPrefix('!');
 
-  }]).value('$anchorScroll', angular.noop);
+//}]).value('$anchorScroll', angular.noop);
+ config(['$provide', '$routeProvider', '$stateProvider', '$urlRouterProvider', function ($provide, $routeProvider, $stateProvider, $urlRouterProvider) {
+
+     $routeProvider
+        .when('/home', { template: '', controller: function () { } })
+        .when('/user', { template: '', controller: function () { } })
+        .when('/mytimeshaft', { template: '', controller: function () { } })
+        .when('/staffmangement', { template: '', controller: function () { } })
+        .when('/changepassword', { template: '', controller: function () { } })
+        .when('/changeemail', { template: '', controller: function () { } })
+        .when('/enterpriseinfo', { template: '', controller: function () { } })
+        .when('/product/:catalogId?/:pageIndex?', { template: '', controller: function () { } })
+        .when('/sales/:steps?/:pageIndex?', { template: '', controller: function () { } })
+        .when('/finance/:steps?/:pageIndex?', { template: '', controller: function () { } })
+        .when('/client/:sorts?/:pageIndex?', { template: '', controller: function () { } })
+        .otherwise({ redirectTo: '/home' });
+
+        $stateProvider
+            .state("main", {
+                url: "",
+                templateUrl: 'partials/main.html'
+            })
+            .state('main.home', {
+                url: '/home',
+                templateUrl: 'partials/home.html',
+                controller: HomeMainCtrl
+            })
+            .state("main.product", {
+                url: "/product*path",
+                templateUrl: 'partials/product.html',
+                controller: ProductMainCtrl
+            })
+            .state('main.user', { url: '/user*path', templateUrl: 'partials/userinfo.html', controller: UserMainCtrl })
+            .state('main.mytimeshaft', { url: '/mytimeshaft*path', templateUrl: 'partials/mytimeshaft.html', controller: UserTimeShaftCtrl })
+            .state('main.staffmangement', { url: '/staffmangement*path', templateUrl: 'partials/staffmangement.html', controller: StaffMangementCtrl })
+            .state('main.changepassword', { url: '/changepassword*path', templateUrl: 'partials/changepassword.html', controller: UserMainCtrl })
+            .state('main.changeemail', { url: '/changeemail*path', templateUrl: 'partials/changeemail.html', controller: UserMainCtrl })
+            .state('main.enterpriseinfo', { url: '/enterpriseinfo*path', templateUrl: 'partials/enterpriseinfo.html', controller: EnterPriseInfoCtrl })
+            .state('main.sales', { url: '/sales*path', templateUrl: 'partials/sales.html', controller: SalesMainCtrl })
+            .state('main.finance', { url: '/finance*path', templateUrl: 'partials/finance.html', controller: FinanceMainCtrl })
+            .state('main.client', { url: '/client*path', templateUrl: 'partials/client.html', controller: ClientMainCtrl })
+    }])
+    .value('$anchorScroll', angular.noop)
+    .run(
+      ['$rootScope', '$state', '$stateParams',
+      function ($rootScope, $state, $stateParams) {
+          $rootScope.$state = $state;
+          $rootScope.$stateParams = $stateParams;
+      }]);;
 
 function MainCtrl($scope, $routeParams, $http, $location, $filter) {
     //退出登录
