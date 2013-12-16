92a48b5248b877aa3168a2ad3d7f67843ba8607d
zhengrunqiang <653260669@qq.com>
2013-12-16 22:52:24 +0800

    xiugai bug

--- MicroAssistant/MicroAssistant.Common/EmailTemplate.cs
+++ MicroAssistant/MicroAssistant.Common/EmailTemplate.cs
@@ -17,7 +17,7 @@ namespace MicroAssistant.Common
                                     "<div style=\"padding:50px 30px 0;color:#333333;\">" +
                                        " <div style=\"margin:0;font-size:14px;padding-top:0;padding-bottom:20px;font-weight:bold;\">亲爱的" + "<a href=\"mailto:" + "{0}" + "\" target=\"_blank\">" + "{0}" + "</a>：</div>" +
                                         "<p style=\"margin:0;text-indent:24px;font-size:12px;padding-top:0;padding-bottom:20px;\">欢迎使用企业助手找回密码功能。</p>" +
-                                        "<p style=\"margin:0;text-indent:24px;font-size:12px;padding-top:0;padding-bottom:20px;\">您此次找回密码的链接是：<a href=\"{1}\">点击此链接修改</a>，请在<b>30分钟内</b>找回密码</p>" +
+                                        "<p style=\"margin:0;text-indent:24px;font-size:12px;padding-top:0;padding-bottom:20px;\">您此次找回密码的链接是：<a href=\"{1}\">点击此处进行修改</a>，请在<b>30分钟内</b>找回密码</p>" +
                                         "<p style=\"margin:0;text-indent:24px;font-size:12px;padding-top:0;padding-bottom:20px;\">如果您并未发过此请求，则可能是因为其他用户在尝试重设密码时误输入了您的电子邮件地址而使您收到这封邮件，那么您可以放心的忽略此邮件，无需进一步采取任何操作。</p>" +
                                        " <p style=\"margin:0;text-indent:24px;font-size:12px;padding-top:0;padding-bottom:20px;\">此致</p>" +
                                         "<p style=\"margin:0;text-indent:24px;font-size:12px;padding-top:0;\">企业助手全体员工敬上</p>" +
