f4823c4d5926eb2469b135cb40878a0f25f79928
kbtyc <66954011@qq.com>
2013-12-28 13:38:05 +0800

    提交企业扩展信息以及用户扩展信息后台方法

---
+++ MicroAssistant/MicroAssistant.Meta/MarketingAfter.cs
@@ -0,0 +1,84 @@
+﻿/**
+ * @author yangchao
+ * @email:aaronyangchao@gmail.com
+ * @date: 2013/12/28 12:28:00
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using MicroAssistant.Common;
+
+namespace MicroAssistant.Meta
+{
+    [Serializable]
+    public class MarketingAfter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Int32 IdmarketingAfter
+        { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Int32 ChanceId
+        { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Int32 ContractId
+        { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public String Detail
+        { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Int32 EntId
+        { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime AddTime
+        { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public String OpUserId
+        { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public String OpUserName
+        { get; set; }
+
+
+        /// <summary>
+        /// 从读取器向完整实例对象赋值
+        /// </summary>/// <param name="reader">数据读取器</param>
+        /// <returns>返回本对象实例</returns>
+        public MarketingAfter BuildSampleEntity(IDataReader reader)
+        {
+            this.IdmarketingAfter = DBConvert.ToInt32(reader["idmarketing_after"]);
+            this.ChanceId = DBConvert.ToInt32(reader["chance_id"]);
+            this.ContractId = DBConvert.ToInt32(reader["contract_id"]);
+            this.Detail = DBConvert.ToString(reader["detail"]);
+            this.EntId = DBConvert.ToInt32(reader["ent_id"]);
+            this.AddTime = DBConvert.ToDateTime(reader["add_time"]);
+            this.OpUserId = DBConvert.ToString(reader["op_user_id"]);
+            this.OpUserName = DBConvert.ToString(reader["op_user_name"]);
+            return this;
+        }
+    }
+}
---
+++ MicroAssistant/MicroAssistant.Meta/SysUserExtra.cs
@@ -0,0 +1,77 @@
+﻿/**
+ * @author yangchao
+ * @email:aaronyangchao@gmail.com
+ * @date: 2013/12/28 11:58:32
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using MicroAssistant.Common;
+
+namespace MicroAssistant.Meta
+{
+    [Serializable]
+    public class SysUserExtra
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Int32 IdsysUserExtra
+        { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public String Diploma
+        { get; set; }
+        
+        /// <summary>
+        ///  
+        /// </summary>
+        public String School
+        { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public String Major
+        { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime GraduationTime
+        { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public String Detail
+        { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Int32 SysUserId
+        { get; set; }
+
+
+        /// <summary>
+        /// 从读取器向完整实例对象赋值
+        /// </summary>/// <param name="reader">数据读取器</param>
+        /// <returns>返回本对象实例</returns>
+        public SysUserExtra BuildSampleEntity(IDataReader reader)
+        {
+            this.IdsysUserExtra = DBConvert.ToInt32(reader["idsys_user_extra"]);
+            this.Diploma = DBConvert.ToString(reader["diploma"]);
+            this.School = DBConvert.ToString(reader["school"]);
+            this.Major = DBConvert.ToString(reader["major"]);
+            this.GraduationTime = DBConvert.ToDateTime(reader["graduation_time"]);
+            this.Detail = DBConvert.ToString(reader["detail"]);
+            this.SysUserId = DBConvert.ToInt32(reader["sys_user_id"]);
+            return this;
+        }
+    }
+}
